7287c5929305d580ab437c7465d523365719d675
mwoodpatrick <mwoodpatrick@gmail.com>
2024-06-19 06:02:40 -0700

    Meta-Exploration — Creating a CLI Package and Self-Testing Flags

--- cmd/files.go
+++ cmd/files.go
@@ -5,18 +5,23 @@ Copyright © 2024 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
-	"fmt"
+     log "github.com/sirupsen/logrus"
 
-	"github.com/spf13/cobra"
+    "github.com/spf13/cobra"
+    "github.com/spf13/viper"
 )
 
 // filesCmd represents the files command
 var filesCmd = &cobra.Command{
  Use:   "files",
  Short: "Show the largest files in the given path.",
- Long: `Quickly scan a directory and find large files.`,
+ Long:  `Quickly scan a directory and find large files.`,
  Run: func(cmd *cobra.Command, args []string) {
-  fmt.Println("files called")
+  for key, value := range viper.GetViper().AllSettings() {
+   log.WithFields(log.Fields{
+    key: value,
+   }).Info("Command Flag")
+  }
  },
 }
 
