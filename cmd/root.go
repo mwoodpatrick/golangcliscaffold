925ab87eadafd61e036ee928f03c87bceae06bb2
mwoodpatrick <mwoodpatrick@gmail.com>
2024-06-19 05:11:58 -0700

    Initial version

---
+++ cmd/root.go
@@ -0,0 +1,42 @@
+/*
+Copyright © 2024 NAME HERE mwoodpatrick@gmail.com
+
+*/
+package cmd
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+
+
+// rootCmd represents the base command when called without any subcommands
+var rootCmd = &cobra.Command{
+ Use:   "getsize",
+ Short: "List the size of a local directory.",
+ Long: `This command will display the size of a directory with several different options.`,
+}
+
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	err := rootCmd.Execute()
+	if err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	// Here you will define your flags and configuration settings.
+	// Cobra supports persistent flags, which, if defined here,
+	// will be global for your application.
+
+	// rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.golangcliscaffold.yaml)")
+
+	// Cobra also supports local flags, which will only run
+	// when this action is called directly.
+	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+
